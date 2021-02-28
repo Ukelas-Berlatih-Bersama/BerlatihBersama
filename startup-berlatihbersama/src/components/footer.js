fd04a1980503626ce28be20d84c94cc0e8b43550
Dian <dian@petanikode.com>
2021-02-28 19:50:08 +0800

    update footer style

--- startup-berlatihbersama/src/components/footer.js
+++ startup-berlatihbersama/src/components/footer.js
@@ -3,9 +3,7 @@ import { Container, Box, Typography } from "@material-ui/core";
 const Footer = function (props) {
   const year = new Date();
   return (
-    <footer
-      style={{ position: "absolute", bottom: 40, left: 0, width: "100%" }}
-    >
+    <footer style={{paddingTop: 32, paddingBottom: 32,fontSize: '.7rem', color: '#6B7380'}}>
       <Container maxWidth="lg" style={{ textAlign: "center" }}>
         <Typography>
           {year.getUTCFullYear()} &copy; Berlatihbersama. All rights reserved.
